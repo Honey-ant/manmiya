a70462a00ca5e87ece7e9fb97c698c1107277398
obby.bedford@gmail.com <obby.bedford@gmail.com>
2022-01-28 15:14:45 +1100

    ain

--- client/src/pages/ArtBio.js
+++ client/src/pages/ArtBio.js
@@ -14,34 +14,39 @@ const ArtBio = () => {
              
           </p>
       </div>
-      <h4 className="title " >Artist Bio</h4>
-      <div className="float-rightt ">
+      <h4 className="title bio" >Artist Bio</h4>
+      <div className="float-rightt overflow-auto">
           <div className="border">
            
             <p>
-                A self taught artist emerging. They are a visual artist 
+                A self taught emerging artist . They are a visual artist 
                 primarily working with acrylic paints and oils. 
+            </p>
+            <p>
                 Their art is an expression of their inner most thoughts, 
                 healing and emotional processing. 
                 Many of their works come to life quickly as the urge takes hold
-                to create, to express.
-                Their style is a mixture of their own Cultural Idenity as a Bunuba Woman,
-                intertwined with all the complexities of living in the colony.
+                to create, to express, to alleviate.
+            </p>
+            <p>
+                Their style is a mixture of inspriation of their own Cultural Idenity as a Bunuba Woman, 
+                that coming from all the deadly artists at <a href="http://www.mangkaja.com/" target="_blank">Mangkaja</a> (art center from Fitzroy Crossing), and
+                intertwined with all the complexities of living in this modern colony.
             </p>
           </div>
         </div>
     </div>
     <img src={giki} className=" float-leftt " alt=" Responsive image" /> 
     <div className="container">
-    <div className=" mb-3 py-3 my-3">
+    <div className=" mb-3 py-3 my-3 absolute">
         <p className="my-2">
       
         </p>
-        <p className="">
+        {/* <p className="">
           They currently live on Boon Wurrong land
-        </p>
+        </p> */}
         <p>
-          Read about them <Link to="/about">here</Link>
+          Read more about them <Link to="/about">here</Link>
         </p>
       </div>
       <Link className="my-3" to="/">← Back to main page</Link>
