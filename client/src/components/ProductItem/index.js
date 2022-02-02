a08c5bdeeaa39f1a15fc4c1bae8229e3c4f6b3cb
obby.bedford@gmail.com <obby.bedford@gmail.com>
2022-02-02 17:52:42 +1100

    ain

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { pluralize } from '../../utils/helpers';
-import { useDispatch, useSelector } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
+import { useReduxStore } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import { idbPromise } from '../../utils/helpers';
 import './style.css';
 
 function ProductItem(item) {
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
+  // const dispatch = useDispatch();
+  // const state = useSelector((state) => state);
+  const [ state, dispatch ] = useReduxStore();
 
   const { image, name, _id, price, quantity } = item;
 
@@ -42,9 +44,9 @@ function ProductItem(item) {
         <h4 className="py-3 ">{name}</h4>
       </Link>
       <div>
-        {/* <div>
+        <div>
           {quantity} {pluralize('item', quantity)} in stock
-        </div> */}
+        </div>
         {/* <span>${price}</span> */}
       </div>
       {/* <button onClick={addToCart}>Add to cart</button> */}
