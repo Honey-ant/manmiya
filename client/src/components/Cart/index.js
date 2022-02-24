20388c0231012c970cd7e6d6c65bb68cbe594854
obby.bedford@gmail.com <obby.bedford@gmail.com>
2022-02-24 17:13:24 +1100

    ain

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -12,7 +12,7 @@ import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
 
-const stripePromise = loadStripe('pk_test_51JXeTvQR8ZQnmKpPpGZYIANsAd55jusCiji4eR5L6nZUyZkbLrhxmIcuNEsQLtqaNMiROEDzrYgG7pctE68yJcwQ00w5WUMtXz');
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
 
