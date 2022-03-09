416f2eb4c77117ed3ab81bed1ee46ff6832fc329
obby.bedford@gmail.com <obby.bedford@gmail.com>
2022-03-09 14:14:10 +0530

    ain

--- client/src/pages/Exhibtions.js
+++ client/src/pages/Exhibtions.js
@@ -12,15 +12,23 @@ export const Exhibtions = () => {
         <div className="container">
             <h3>Exhibtions</h3> 
             <p>
-                My exhibtion count thus far is at a exciting one (1).
-               I have COVID to thank, as I was meant to be apart of some last year but were sadly cancelled.
-
+                My debut exhibition was in January 2022 in Naarm (Melbourne Australia).
+                This was were a fantastic collection of artists called the 'Kiss my Art Collective'.
+                
+            </p>
+            <p>
+                My second and current exhibition is an online fundraising event that I am so lucky and gratful to be apart of.
+                The fundraiser is for the 'Art of Healing' foundation, that is based in Canada.
+                This foundation was founded by my therapist Atira Tan. 
+                Someone I am in awe of for so many reasons. 
             </p>
             <div className="inner">
                 <div className="mx-3 py-3 px-3">
-                    
-                    <h5>The Kiss my Art Collective!</h5>
+                <br></br>
+                    <h5 className="center">The Kiss my Art Collective!</h5>
+                    <br></br>
                     <img src={kiss} className="pro img   " alt="Card image cap  " />  
+                    <br></br>
                     <p className=" py-3 px-3">
                         The Kiss my art collective is made up of all femme people. Mainly poeple of colour (which we love to see). We have all been brought together by helping with muruals for the deadly Aretha Brown.
                     </p>
@@ -34,10 +42,40 @@ export const Exhibtions = () => {
                     </p>
                     
                     <img src={exb} className="pro img   " alt="Card image cap  " />  
-                    <Link className="py-4 my-3 px-3" to="/contact">Contact me to inquire about them</Link>
+                    <br></br>
+                    <Link className="py-4 my-3 px-3 center" to="/contact">Contact me to inquire about them</Link>
                 </div>
             </div>
+            <br></br>
+            
+            <br></br>
+
+            <div className="inner">
+                <div className="mx-3 py-3 px-3">
+                <br></br>
+                    <h5>The Art of Healing</h5>
+                    <br></br>
+                    <p>
+                        An amazingly appropriate name for all of the work the founders
+                         and workers do for so many poeple around the world.
+                    </p>
+                    <p>
+                        The work the foundation does is centered around helping people who have been sold into
+                        sex slavery, and aids them in their recovery.
+                    </p>
+                    <p>
+                        All of the art works in the collctive were created after therapy sessions when I have felt so 
+                        centered and aligened.
+                    </p>
+                    <p>
+                        
+                        <a href="https://www.arttohealing.org/art-fundraiser-2022/" target="_blank">Art of healing Fundraiser</a>
+                    </p>
+                </div>
+            </div>
+            <br></br>
             <Link className="my-2" to="/">← Back to main page</Link>
+            <br></br>
         </div>
         </>
     )
