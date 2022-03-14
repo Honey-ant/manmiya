47e6e45357e63cc1fe419180352a85a73aac2af9
obby.bedford@gmail.com <obby.bedford@gmail.com>
2022-03-14 18:03:40 +0530

    ain

--- client/src/pages/ArtBio.js
+++ client/src/pages/ArtBio.js
@@ -15,29 +15,30 @@ const ArtBio = () => {
         </div>
         <h4 className="title bio" >Artist Bio</h4>
         <div className="float-rightt overflow-auto">
-            <div className="border inner">
-            
-              <p>
-                  A self taught emerging artist . They are a visual artist 
-                  primarily working with acrylic paints and oils. 
-              </p>
-              <p>
-                  Their art is an expression of their inner most thoughts, 
-                  healing and emotional processing. 
-                  Many of their works come to life quickly, as the urge takes hold
-                  to create, to express, to alleviate.
-              </p>
-              <p>
-                  Their style is a mixture of inspriation of their own Cultural Idenity as a Bunuba Woman, 
-                  that coming from all the deadly artists at <a href="http://www.mangkaja.com/" target="_blank">Mangkaja</a> (art center from Fitzroy Crossing), and
-                  intertwined with all the complexities of living in this modern colony.
-              </p>
+          <div className="boxx">
+            <div className="px-2">
+                <p>
+                    A self taught emerging artist . They are a visual artist 
+                    primarily working with acrylic paints and oils. 
+                </p>
+                <p>
+                    Their art is an expression of their inner most thoughts, 
+                    healing and emotional processing. 
+                    Many of their works come to life quickly, as the urge takes hold
+                    to create, to express, to alleviate.
+                </p>
+                <p>
+                    Their style is a mixture of inspriation of their own Cultural Idenity as a Bunuba Woman, 
+                    that coming from all the deadly artists at <a href="http://www.mangkaja.com/" target="_blank">Mangkaja</a> (art center from Fitzroy Crossing), and
+                    intertwined with all the complexities of living in this modern colony.
+                </p>
+              </div>
             </div>
           </div>
       </div>
-      <img src={giki} className=" float-leftt emg" alt=" Responsive image" /> 
+      <img src={giki} className=" fit " alt=" Responsive image" /> 
       <div className="container">
-      <div className="mb-3 py-3 my-3 ">
+      <div className="">
   
           <p className="my-4 read">
             Read more about them <Link to="/about">here</Link>
