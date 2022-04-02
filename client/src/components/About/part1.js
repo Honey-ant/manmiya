1560c82e1d4b54356f3262860258fe733edd35f1
obby.bedford@gmail.com <obby.bedford@gmail.com>
2022-04-02 16:02:01 +0530

    ain

--- client/src/components/About/part1.js
+++ client/src/components/About/part1.js
@@ -10,11 +10,12 @@ export const Part1 = () => {
                             {/* <Link className="my-3 py-3 mx-4" to="/">← Back to main page</Link> */}
                             <h3 className="  container title " id="">About</h3>
                             <hr className="container"></hr> 
-                           <div className="box py-3 my-2 glow">  
-                            <img src={giki} className="right pro img" alt=" Responsive image Card image cap" />  
+                           <div className="box glow">  
+                            <img src={giki} className=" pro img py-2" alt=" Responsive image Card image cap" />  
 
-                            <div className="container " >
-                                <div className="flo-left  py-2 ">
+                            <div className="container glow" >
+                                <div className="flo-left right my-1 shade rounded glow">
+                                   
                                    <p className=" m-3 "> 
                                         
                                          Manmiya is her Aboriginal name given from her grandparents.
@@ -23,12 +24,12 @@ export const Part1 = () => {
                                          They pick names from five generations ago.
                                         
                                     </p>
-                                    <hr className="white"></hr> 
+                                    
                                 </div>
                             </div> 
                              </div>
                             <hr className="container"></hr>
-                        {/* <br></br> */}
+                        <br></br>
                              
             </div> 
         </>
