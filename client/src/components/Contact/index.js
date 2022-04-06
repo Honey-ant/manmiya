3ab6706258c2b01a5fab8b0a07b2a784f2bd6978
obby.bedford@gmail.com <obby.bedford@gmail.com>
2022-04-06 15:29:28 +0530

    ain

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -41,42 +41,42 @@ export const Contact = () => {
         </h4>
      
         <hr className="container"></hr>
-        <div className="box py-2 container">
-          <form className="form container " ref={form} onSubmit={sendEmail}>
-          <div className="flex-row space-between my-2 py-2 ">
-            <label className="my-2" htmlFor="firstName">Name:</label>
-            <input
-              placeholder=" Your Name"
-              name="from_name"
-              type="firstName"
-              id="firstName"
-              
-            />
-          </div>
-          <div className="flex-row space-between my-2 py-2 ">
-            <label className="my-2" htmlFor="email">Email:</label>
-            <input
-              placeholder="youremail@email.com"
-              name="reply_to"
-              type="email"
-              id="email"
-              
-            />
-          </div>
+        <div className="box py-2 container glow rounded">
+          <form className="form container rounded" ref={form} onSubmit={sendEmail}>
+            <div className="flex-row space-between my-2 py-2 ">
+              <label className="my-2" htmlFor="firstName">Name:</label>
+              <input
+                placeholder=" Your Name"
+                name="from_name"
+                type="firstName"
+                id="firstName"
+                
+              />
+            </div>
+            <div className="flex-row space-between my-2 py-2 ">
+              <label className="my-2" htmlFor="email">Email:</label>
+              <input
+                placeholder="youremail@email.com"
+                name="reply_to"
+                type="email"
+                id="email"
+                
+              />
+            </div>
 
-          <div className="flex-row space-between my-2 py-2 ">
-            <label className="my-2" htmlFor="email">Message:</label>
-            <textarea
-              placeholder="Your message"
-              name="message"
-              type="messsage"
-              id="message"
-            
-            />
-          </div>
-          <div className="flex-row flex-end py-4">
-            <button type="submit pink">Submit</button>
-          </div>
+            <div className="flex-row space-between my-2 py-2 ">
+              <label className="my-2" htmlFor="email">Message:</label>
+              <textarea
+                placeholder="Your message"
+                name="message"
+                type="messsage"
+                id="message"
+              
+              />
+            </div>
+            <div className="flex-row flex-end py-4">
+              <button className="shade white button-74" type="submit pink">Submit</button>
+            </div>
           </form>
         </div>
         <hr className="container"></hr>
