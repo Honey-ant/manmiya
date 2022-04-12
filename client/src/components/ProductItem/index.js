857cf323cbce5a99adbef94b84e8b567dc15041d
obby.bedford@gmail.com <obby.bedford@gmail.com>
2022-04-12 15:09:00 +0530

    ain

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { pluralize } from '../../utils/helpers';
-import { useDispatch, useSelector } from 'react-redux';
-// import { useStoreContext } from "../../utils/GlobalState";
+import { useReduxStore } from "../../utils/GlobalState";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import { idbPromise } from '../../utils/helpers';
 import './style.css';
 
 function ProductItem(item) {
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
-  // const [ state, dispatch ] = useStoreContext();
+  const [state, dispatch] = useReduxStore();
 
   const { image, name, _id, price, quantity } = item;
 
